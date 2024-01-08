054f3d8de35c1577cc725b07564e25cc4f8c5f0d
Hirukamunasinghe <munasinghehiruka@gmail.com>
2024-01-08 15:56:37 +0530

    commit

--- properties-web-react/src/App.jsx
+++ properties-web-react/src/App.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import './App.css';
-
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import "./App.css";
 
 function App() {
+  const [filteredData, setFilteredData] = useState(null);
+  const [advancedSearchCriteria, setAdvancedSearchCriteria] = useState(null);
+
   return (
-    <div className='App'>
-      <Navbar />
-    </div>
-  )
+    <Router>
+      <div>
+        <Navbar
+          setFilteredData={setFilteredData}
+          setAdvancedSearchCriteria={setAdvancedSearchCriteria}
+        />
+        <Routes>
+          <Route
+            path="/"
+            element={<Home filteredData={filteredData} advancedSearchCriteria={advancedSearchCriteria} />}
+          />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- properties-web-react/src/components/Navbar.jsx
+++ properties-web-react/src/components/Navbar.jsx
@@ -1,63 +1,197 @@
 import React from "react";
+// importing router from react-router-dom for navigations
 import { useState } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 
 
-const Navbar =() => {
-    const [title, setTitle] = useState('');
-    const [isActive, setIsActive] = useState(false);
-    const [isSearchActive, setIsSearchActive] = useState(false);
-    const [isDropdownActive, setIsDropdownActive] = useState(false);
+const Navbar = ({ setFilteredData,setAdvancedSearchCriteria }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [title, setTitle] = useState("");
+  const [isActive, setIsActive] = useState(false);
+  
+  const [isSearchActive, setIsSearchActive] = useState(false);
+  const [isDropdownActive, setIsDropdownActive] = useState(false);
 
-    const handleToggle = () => {
-        setIsActive(!isActive);
-        setIsSearchActive(!isSearchActive);
-        setIsDropdownActive(!isDropdownActive);
+
+
+  const handleToggle = () => {
+    setIsActive(!isActive);
+    setIsSearchActive(!isSearchActive);
+    setIsDropdownActive(!isDropdownActive);
+  };
+
+  const handleSearch = (e) => {
+    const searchTerm = e.target.value.toLowerCase();
+    setFilteredData(searchTerm); // Pass the search term to the parent component
+  };
+
+
+
+  const scrollToCardsSection = () => {
+    const cardsSection = document.querySelector("#cards-section");
+    if (cardsSection) {
+      cardsSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  // advanced search features
+  const [showAdvancedSearch, setShowAdvancedSearch] = useState(false);
+  const [type, setType] = useState("Any");
+  const [minPrice, setMinPrice] = useState("");
+  const [maxPrice, setMaxPrice] = useState("");
+  const [minBedrooms, setMinBedrooms] = useState("");
+  const [maxBedrooms, setMaxBedrooms] = useState("");
+
+  const handleToggleAdvancedSearch = () => {
+    setShowAdvancedSearch(!showAdvancedSearch);
+  };
+
+  const advhandleSearch = () => {
+    const searchCriteria = {
+      type,
+      minPrice,
+      maxPrice,
+      minBedrooms,
+      maxBedrooms,
     };
+    setAdvancedSearchCriteria(searchCriteria);
+    setShowAdvancedSearch(false);
+  };
 
-    return(
-        <nav className={`navbar ${isActive ? 'active' : ''}`}>
-            <div className="textContent">
-                <h1 className="navbarTitle">Propz7</h1>
-                <div className={`searchDiv ${isSearchActive ? 'active' : ''}`}>
-                <input className="searchInput" placeholder="search" type="search" value={title} onChange={(e)=>setTitle(e.target.value)}></input>
-                <FontAwesomeIcon className="fas" icon={faSearch} />
-                </div>
-            </div>
-            <div className={`listContent ${isActive ? 'active' : ''}`}>
-                <ul className={`uList ${isActive ? 'active' : ''}`}>
-                    <div className={`dropdown ${isDropdownActive ? 'active' : ''}`}>
-                        <a  className={`dropbtn ${isDropdownActive ? 'active' : ''}`}>Buy <FontAwesomeIcon className="car" icon={faCaretDown} /></a>
-                        <div className="dropdown-content">
-                        <a class="drop-content-link" href="#">Property for Sale</a>
-                        <a class="drop-content-link" href="#">Homes for Sale</a>
-                        <a class="drop-content-link" href="#">Property Valuation</a>
-                        <a class="drop-content-link" href="#">Where can I live</a>
-                        </div>
-                        </div>
-                        <div className={`dropdown ${isActive ? 'active' : ''}`}>
-                        <a  className={`dropbtn ${isDropdownActive ? 'active' : ''}`}>Rent <FontAwesomeIcon className="car" icon={faCaretDown} /></a>
-                        
-                        <div class="dropdown-content">
-                        <a class="drop-content-link" href="#">Property to Rent</a>
-                        <a class="drop-content-link" href="#">Student Property to Rent</a>
-                        <a class="drop-content-link" href="#">Where can I live</a>
-                        </div>
-                        </div>
-                    <li className={`nav-li ${isActive ? 'active' : ''}`}><a href="">House Prices</a></li>
-                    <li className={`nav-li ${isActive ? 'active' : ''}`}><a href="">Our Services</a></li>
-                </ul>
-            </div>
-             <a href="#" className="toggle-button" onClick={handleToggle}>
-                <span class="toggle-bar"></span>
-                <span class="toggle-bar"></span>
-                <span class="toggle-bar"></span>
+  const handleTypeChange = (e) => {
+    setType(e.target.value);
+  };
+
+  const handleMinPriceChange = (e) => {
+    setMinPrice(e.target.value);
+  };
+
+  const handleMaxPriceChange = (e) => {
+    setMaxPrice(e.target.value);
+  };
+
+  const handleMinBedroomsChange = (e) => {
+    setMinBedrooms(e.target.value);
+  };
+
+  const handleMaxBedroomsChange = (e) => {
+    setMaxBedrooms(e.target.value);
+  };
+
+  const handleClear = () => {
+    setType("Any");
+    setMinPrice("");
+    setMaxPrice("");
+    setMinBedrooms("");
+    setMaxBedrooms("");
+    setAdvancedSearchCriteria(null);
+    setShowAdvancedSearch(false);
+  };
+
+
+  return (
+    <nav className={`navbar ${isActive ? "active" : ""}`}>
+      <div className="textContent">
+        <h1 className="navbarTitle">Propz7</h1>
+        <div className={`searchDiv ${isSearchActive ? "active" : ""}`}>
+          <input
+            className="searchInput"
+            placeholder="search by type -: house,apartment..."
+            type="search"
+            onChange={handleSearch}
+          ></input>
+          <FontAwesomeIcon  className="fas" icon={faSearch} onClick={scrollToCardsSection} />
+          <button className="advsearchbtn" onClick={handleToggleAdvancedSearch} >Advanced Search
+          </button>
+          {showAdvancedSearch && (
+        <div className="advanced-search-dialog">
+          <h2>Advanced Search</h2>
+          <label>
+            Type:
+            <select value={type} onChange={handleTypeChange}>
+              <option value="Any">Any</option>
+              <option value="House">House</option>
+              <option value="Apartment">Apartment</option>
+              <option value="Villa">Villa</option>
+            </select>
+          </label>
+          <label>
+            Min Price:
+            <input type="text" value={minPrice} onChange={handleMinPriceChange} />
+          </label>
+          <label>
+            Max Price:
+            <input type="text" value={maxPrice} onChange={handleMaxPriceChange} />
+          </label>
+          <label>
+            Min Bedrooms:
+            <input type="text" value={minBedrooms} onChange={handleMinBedroomsChange} />
+          </label>
+          <label>
+            Max Bedrooms:
+            <input type="text" value={maxBedrooms} onChange={handleMaxBedroomsChange} />
+          </label>
+          <button onClick={advhandleSearch} className="adv-searchbtn">Search</button>
+          <button onClick={handleClear} className="adv-searchbtn">Clear</button>
+        </div>
+      )}
+        </div>
+      </div>
+      <div className={`listContent ${isActive ? "active" : ""}`}>
+        <ul className={`uList ${isActive ? "active" : ""}`}>
+          <div className={`dropdown ${isDropdownActive ? "active" : ""}`}>
+            <a className={`dropbtn ${isDropdownActive ? "active" : ""}`}>
+              Buy <FontAwesomeIcon className="car" icon={faCaretDown} />
+            </a>
+            <div className="dropdown-content">
+              <a class="drop-content-link" href="#">
+                Property for Sale
               </a>
-        </nav>
-    )
-}
+              <a class="drop-content-link" href="#">
+                Homes for Sale
+              </a>
+              <a class="drop-content-link" href="#">
+                Property Valuation
+              </a>
+              <a class="drop-content-link" href="#">
+                Where can I live
+              </a>
+            </div>
+          </div>
+          <div className={`dropdown ${isDropdownActive ? "active" : ""}`}>
+            <a className={`dropbtn ${isDropdownActive ? "active" : ""}`}>
+              Rent <FontAwesomeIcon className="car" icon={faCaretDown} />
+            </a>
+            <div class="dropdown-content">
+              <a class="drop-content-link" href="#">
+                Property to Rent
+              </a>
+              <a class="drop-content-link" href="#">
+                Student Property to Rent
+              </a>
+              <a class="drop-content-link" href="#">
+                Where can I live
+              </a>
+            </div>
+          </div>
+          <li className={`nav-li ${isActive ? "active" : ""}`}>
+            <a href="#cards-section">House Prices </a>
+          </li>
+          <li className={`nav-li ${isActive ? "active" : ""}`}>
+            <a href="">Our Services</a>
+          </li>
+        </ul>
+      </div>
+      <a href="#" className="toggle-button" onClick={handleToggle}>
+        <span class="toggle-bar"></span>
+        <span class="toggle-bar"></span>
+        <span class="toggle-bar"></span>
+      </a>
+    </nav>
+    
+  );
+};
 
-
-export default Navbar
+export default Navbar;
